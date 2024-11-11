f3bb412817b4498c9d2c423d9c21e16cd53fee37
banagaurav <gauravbana840@gmail.com>
2024-11-11 19:00:38 +0545

    renamed Academics to Blogs

--- Portfolio/obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ Portfolio/obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9c711e8c7eb81f5e5921ae6510c11323bcf6232")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d13705625893ef83029c9da702e632c22df3ead7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
