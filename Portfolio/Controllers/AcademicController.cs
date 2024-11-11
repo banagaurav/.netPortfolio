e9c711e8c7eb81f5e5921ae6510c11323bcf6232
banagaurav <gauravbana840@gmail.com>
2024-11-11 18:51:37 +0545

    add mvc application, add views/academices

---
+++ Portfolio/Controllers/AcademicController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class AcademicController : Controller
+    {
+        // GET: AcademicController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
