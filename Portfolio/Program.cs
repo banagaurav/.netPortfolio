e21a890cf0b6ae6041f7258c85ae01025d9cff98
banagaurav <gauravbana840@gmail.com>
2024-12-05 12:51:51 +0545

    remove login , redo

--- Portfolio/Program.cs
+++ Portfolio/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Portfolio.Data;
-using Portfolio.Services;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,18 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddHttpContextAccessor();
-
-// Add session services
-builder.Services.AddSession(options =>
-{
-    // options.IdleTimeout = TimeSpan.FromMinutes(30); // Set session timeout
-    options.Cookie.HttpOnly = true;                // Ensure cookie is HTTP-only
-    options.Cookie.IsEssential = true;             // Mark session cookie as essential
-});
-
-// Register application services
-builder.Services.AddScoped<IUserService, UserService>();
 
 var app = builder.Build();
 
@@ -35,20 +23,12 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-// Add session middleware
-app.UseSession();
-
 app.UseAuthorization();
 
 // Map area-specific routes
 app.MapControllerRoute(
     name: "areas",
     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
-
-// Map default route
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.MapControllerRoute(
     name: "default",
--- Portfolio/obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ Portfolio/obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dea4cf5775f07a2fd620ccbf01fc30494210038b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c2ed59e63b9a0f8b791ba728f1103fe98e8df89")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
